27d9db8d8f513e92004da2a1301718ab75808bd1
cnlohr <lohr85@gmail.com>
2025-02-12 17:15:04 -0800

    semi still not quite working.

--- schtest/combotest/combotest-semi.c
+++ schtest/combotest/combotest-semi.c
@@ -6,35 +6,35 @@ int main()
 	// Testing for a setup like:
 	//   (R+C) for all capacitors.
 	//   Possibly (R1||(R2+C))
+	// This method works well.
 
 	float tdelta = 1.0e-12;
 	float mintargetnodecap = 1.0e-12;
 
-	#define NODES 6
-	#define COMPS 5
+	#define NODES 5
+	#define COMPS 2
 	float NodeVoltages[NODES] = { 0 };
 	float ComponentCurrents[COMPS] = { 0 };
 
 	// Instantaneous I deltas
 	float NodeIs[NODES] = { 0 };
 
-	const float ComponentRs[COMPS] = { 2, 1, 1000, 1000, 1000 };
-	const float ComponentCs[COMPS] = { -1, 30.e-12, -1, 1.e-12, -1 };
+	const float ComponentRs[COMPS] = { 1, 100 };
+	const float ComponentCs[COMPS] = { 100.e-12, -1 };
 	float ComponentVDiff[COMPS] = { 0 };
 	int ComponentTerms[COMPS][2] = {
 		{ 0, 1 },
-		{ 1, 2 },
-		{ 2, 3 },
-		{ 2, 4 },
-		{ 4, 5 } };
+		{ 3, 4 },
+	};
 	float NodeCaps[NODES] = { 0 };
 	
 	int c;
-	for( c = 0; c < COMPS; c++ )
-	{
-		NodeCaps[ComponentTerms[c][0]] += tdelta / .99 / ComponentRs[c];
-		NodeCaps[ComponentTerms[c][1]] += tdelta / .99 / ComponentRs[c];
-	}
+	NodeCaps[0] = 10000;
+	NodeCaps[1] = 2.e-12;
+	NodeCaps[2] = 10000;
+	NodeCaps[3] = 2.e-12;
+	NodeCaps[4] = 10000;
+	
 	int n;
 	for( n = 0; n < NODES; n++ )
 	{
@@ -42,13 +42,16 @@ int main()
 			NodeCaps[n] = mintargetnodecap;
 		//printf( "NC %d %f\n", n, NodeCaps[n]*1.e12 );
 	}
+	
+	float MOSFetGateCharge = 0.0;
+	float MOSFetVSD = 0.0;
 
 	int iteration;
-	for( iteration = 0; iteration < 10000; iteration++ )
+	for( iteration = 0; iteration < 1000; iteration++ )
 	{
-		NodeVoltages[0] = (sin(iteration/100.0) * 2.50) > 0.0 ? 5.0 : 0.0;
-		NodeVoltages[3] = 0.0;
-		NodeVoltages[5] = 5.0;
+		NodeVoltages[0] = (sin(iteration/300.0) * 2.50) > 0.0 ? 5.0 : 0.0;
+		NodeVoltages[2] = 0.0;
+		NodeVoltages[4] = 5.0;
 		for( n = 0; n < NODES; n++ )
 		{
 			NodeIs[n] = 0;
@@ -75,6 +78,58 @@ int main()
 			ComponentVDiff[c] = cdiff;
 			//printf( "%f,%f,%f,+%c", deltaI, cdiff, vDiff, (c == COMPS-1) ? '\n':',' );
 		}
+		
+		// Emulate mosfet.
+		{
+			int nG = 1;
+			int nS = 2;
+			int nD = 3;
+			float vG = NodeVoltages[nG];
+			float vS = NodeVoltages[nS];
+			float vD = NodeVoltages[nD];
+			float gQ = MOSFetGateCharge;
+
+			const float cISS = 15.e-12;
+			const float cOSS = 15.e-12;
+			const float cRSS = 15.e-12; // TODO.
+			const float vGSth = 1.6;
+			const float rDSOn = 5;
+			const float rG = 2;
+
+			// Computed based on gQ.
+			float vGS = gQ / cISS;
+			float rFET = 1.e6;
+			
+			// Pretend: Pretend the miller plateau is nearly flat.
+			if( vGS > vGSth )
+			{
+				const float plateauSize = vGSth;
+				float vGSTmp = (vGS - vGSth);
+				rFET = rFET / (vGSTmp+1);
+				if( vGSTmp < plateauSize )
+				{
+					vGSTmp = 0;
+				}
+				else
+				{
+					vGSTmp -= plateauSize;
+				}
+				vGS = vGSth + vGSTmp;
+			}
+			
+			float iSD = (vD - vS + MOSFetVSD) / rFET;
+			float iG = (vG - vS - vGS) / rG;
+			printf( "[%f-%f] VGS: %f (%f) -> %f\n", vG, vS, vGS, iG, rFET );
+			
+			MOSFetGateCharge += iG * tdelta;
+			MOSFetVSD += iSD / cOSS * tdelta;
+			
+			NodeIs[nS] += iG/2;
+			NodeIs[nG] -= iG/2;
+			NodeIs[nS] += iSD/2;
+			NodeIs[nD] -= iSD/2;
+		}
+		
 		
 		for( n = 0; n < NODES; n++ )
 		{
